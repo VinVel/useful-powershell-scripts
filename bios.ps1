0200c43883faff0d771ae56693e18cd294425b3b
unknown <git@velcore.net>
2026-01-11 13:03:04 +0100

    made some changes

--- bios.ps1
+++ bios.ps1
@@ -1,4 +1,4 @@
-#windows only, reboots into the UEFI (not legacy bios)
+# windows only, reboots into the UEFI (not legacy bios)
 
 Write-Host "System wird sofort neu gestartet und öffnet die UEFI-Firmware."
-shutdown /r /fw /f /t 0
+sudo shutdown.exe /r /fw
