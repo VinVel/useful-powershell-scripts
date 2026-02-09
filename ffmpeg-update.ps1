3f02ea2e94ca4e664831768876fdbbe3d075c833
git <git@velcore.net>
2026-02-09 19:11:05 +0100

    change ffmpeg updater to not use the shared libraries

--- ffmpeg-update.ps1
+++ ffmpeg-update.ps1
@@ -1,6 +1,6 @@
 # ================== Konfiguration ==================
 $RepoApiUrl   = "https://api.github.com/repos/BtbN/FFmpeg-Builds/releases/latest"
-$AssetName    = "ffmpeg-master-latest-win64-gpl-shared.zip"
+$AssetName    = "ffmpeg-master-latest-win64-gpl.zip"
 $BinTarget    = "C:\Users\vinvel\usr-bin"
 $StateFile    = "$PSScriptRoot\last_release.txt"
 $TempDir      = "$PSScriptRoot\ffmpeg_tmp"
